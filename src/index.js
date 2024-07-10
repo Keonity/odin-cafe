fd16a79326d4dad1b84576bb72a5e2404a8f5d9d
Keonity <56704285+Keonity@users.noreply.github.com>
2024-07-10 04:45:26 -0700

    Changed background image, added story text and styling to homepage.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ console.log("Test");
 // alert("Alert");
 
 const navBar = document.querySelector("#nav");
-const cafeTitle = document.createElement("h3");
+const cafeTitle = document.createElement("h1");
 cafeTitle.setAttribute("id", "cafeTitle");
 cafeTitle.innerHTML = "Verdant Cafe";
 navBar.appendChild(cafeTitle);
@@ -15,3 +15,20 @@ navBar.appendChild(navBtnContainer);
 navBtnContainer.append(document.querySelector("#homeBtn"));
 navBtnContainer.append(document.querySelector("#menuBtn"));
 navBtnContainer.append(document.querySelector("#aboutBtn"));
+
+const contentContainer = document.querySelector("#content");
+const tagline = document.createElement("h3");
+tagline.setAttribute("id", "tagline");
+tagline.innerHTML = "\"Mother Terra's Favorite Coffee!\"";
+contentContainer.appendChild(tagline);
+
+const storyContainer = document.createElement("div");
+storyContainer.setAttribute("id", "storyContainer");
+
+const storyText = document.createElement("p");
+const storyTextNode = document.createTextNode("Serving Honeymoon Bay with assorted coffee, tea, and artisinal sandwiches since 2012. Verdant Cafe is owned by brothers Joey and Jay, two nationally recognized coffee connoiseurs.");
+storyText.appendChild(storyTextNode);
+storyText.setAttribute("id", "storyText");
+storyContainer.appendChild(storyText);
+
+contentContainer.appendChild(storyContainer);
