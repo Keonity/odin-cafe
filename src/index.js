ec3a803eeac8c2990c23df327e551586bff6c260
Keonity <56704285+Keonity@users.noreply.github.com>
2024-07-10 00:06:57 -0700

    Added basic home page styling.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import './style.css';
 
 console.log("Test");
-alert("Alert");
+// alert("Alert");
+
+const navBar = document.querySelector("#nav");
+const cafeTitle = document.createElement("h3");
+cafeTitle.setAttribute("id", "cafeTitle");
+cafeTitle.innerHTML = "Verdant Cafe";
+navBar.appendChild(cafeTitle);
+
+const navBtnContainer = document.createElement("div");
+navBtnContainer.setAttribute("id", "navBtnContainer");
+navBar.appendChild(navBtnContainer);
+navBtnContainer.append(document.querySelector("#homeBtn"));
+navBtnContainer.append(document.querySelector("#menuBtn"));
+navBtnContainer.append(document.querySelector("#aboutBtn"));
